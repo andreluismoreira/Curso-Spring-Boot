6d394100ade45f41be738dfff6f7f72f6b10e94e
andreluismoreira <andreluistma@gmail.com>
2021-09-15 13:45:06 -0300

    Implementação do bd h2 para testes

--- src/main/java/programWebServices/Application.java
+++ src/main/java/programWebServices/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootNelioAlvesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootNelioAlvesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
