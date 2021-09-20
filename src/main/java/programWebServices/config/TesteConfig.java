29a848c7513c92f810282d84597ed5653726155c
andreluismoreira <andreluistma@gmail.com>
2021-09-20 18:59:56 -0300

    OrderItem many-to-Many Association with extra attributes

--- src/main/java/programWebServices/config/TesteConfig.java
+++ src/main/java/programWebServices/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import programWebServices.entities.Category;
 import programWebServices.entities.Order;
+import programWebServices.entities.OrderItem;
 import programWebServices.entities.Product;
 import programWebServices.entities.User;
 import programWebServices.entities.enums.OrderStatus;
 import programWebServices.repositories.CategoryRepository;
+import programWebServices.repositories.OrderItemRepository;
 import programWebServices.repositories.OrderRepository;
 import programWebServices.repositories.ProductRepository;
 import programWebServices.repositories.UserRepository;
@@ -34,6 +36,8 @@ public class TesteConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -46,6 +50,18 @@ public class TesteConfig implements CommandLineRunner {
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -62,19 +78,12 @@ public class TesteConfig implements CommandLineRunner {
 
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		p1.getCategories().add(cat2);
-		p2.getCategories().add(cat1);
-		p2.getCategories().add(cat3);
-		p3.getCategories().add(cat3);
-		p4.getCategories().add(cat3);
-		p5.getCategories().add(cat2);
-		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		
 	}
