9f55346de6f1f8c2300a8a06132cf60fa9b8388c
andreluismoreira <andreluistma@gmail.com>
2021-10-06 23:58:27 -0300

    pequenas melhorias

--- src/main/java/programWebServices/services/exceptions/DatabaseException.java
+++ src/main/java/programWebServices/services/exceptions/DatabaseException.java
@@ -1,8 +1,6 @@
 package programWebServices.services.exceptions;
 
-import java.io.Serializable;
-
-public class DatabaseException extends RuntimeException implements Serializable  {
+public class DatabaseException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public DatabaseException(String msg) {
